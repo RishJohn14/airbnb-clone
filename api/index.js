626408703f851fc3c2322a68e5d6e30c18eaee35
Rishabh John <johnrishabhalex@gmail.com>
2024-07-29 10:40:35 +0800

    Upload photos by link

--- api/index.js
+++ api/index.js
@@ -5,12 +5,14 @@ const bcrypt = require('bcryptjs');
 const bcryptSalt = bcrypt.genSaltSync(10);
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser'); 
+const imageDownloader = require('image-downloader');
 require('dotenv').config();
 const User = require('./models/User');
 const jwtSecret = 'randomString';
 const app = express(); 
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:5173',
@@ -79,5 +81,16 @@ app.get('/profile', (req,res)=>{
 app.post('/logout', (req,res)=> {
     res.cookie('token','').json(true);
 });
+
+app.post('/upload-by-link', async  (req,res) => {
+    const {link} = req.body;
+    const newName = 'photo' + Date.now() + '.jpg';
+    await imageDownloader.image({
+        url: link,
+        dest: __dirname+'/uploads/' + newName,
+
+    })
+    res.json(newName);
+})
 
 app.listen(4000);
